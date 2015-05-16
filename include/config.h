0af4136c17a1877657dfa224f088d575be207436
martin <martin.gurr@gmail.com>
2015-05-16 16:58:00 +0200

    Enabled autopickup exceptions

--- include/config.h
+++ include/config.h
@@ -361,7 +361,7 @@ typedef unsigned char	uchar;
  */
 
 #define GOLDOBJ	/* Gold is kept on obj chains - Helge Hafting */
-/*#define AUTOPICKUP_EXCEPTIONS */ /* exceptions to autopickup */
+#define AUTOPICKUP_EXCEPTIONS /* exceptions to autopickup */
 
 /* End of Section 5 */
 
